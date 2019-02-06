c739ae2347955c2b8e448110faefeaa24bc809eb
Jack Purdie <jpurdie@live.unc.edu>
2019-02-05 21:06:58 -0500

    Initial Commit

---
+++ ptwiredtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ptwiredtest
+//
+//  Created by Jack Purdie on 2/5/19.
+//  Copyright © 2019 Jack Purdie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ptwiredtestTests/ptwiredtestTests.swift
@@ -0,0 +1,34 @@
+//
+//  ptwiredtestTests.swift
+//  ptwiredtestTests
+//
+//  Created by Jack Purdie on 2/5/19.
+//  Copyright © 2019 Jack Purdie. All rights reserved.
+//
+
+import XCTest
+@testable import ptwiredtest
+
+class ptwiredtestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
